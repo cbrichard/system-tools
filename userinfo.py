be686363b136b51bf219d9bd1408e1e516ad5aa1
Chris Richard <christopher.richard27@gmail.com>
2017-06-04 23:07:20 -0700

    initial commit

---
+++ userinfo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import subprocess
+
+
+users = {}
+ps_cmd = subprocess.check_output(['ps', 'aux'])
+
+for line in ps_cmd.splitlines()[1:]:
+    user = line.split()[0]
+    if users.get(user):
+        users[user]+=1
+    else:
+        users[user]=1
+
+print "Active users on the system are " + ','.join(users.keys())
+
+for user, process_count in users.items():
+    print "%s is running %s processes" % (user, process_count)
