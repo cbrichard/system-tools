86113a7dfeb2f9fee9f13d85444aeb94f0ea3fe7
Chris Richard <christopher.richard27@gmail.com>
2017-08-21 20:15:47 -0700

    added new script to check system processes

--- system-procs.py
+++ system-procs.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python
 import subprocess
 
 users = {}
-ps_cmd = subprocess.check_output(['ps','-ef'])
+ps_cmd = subprocess.check_output(['ps','aux'])
 
 for line in ps_cmd.splitlines()[1:]:
     user = line.split()[0]
